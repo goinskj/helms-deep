2215701e4539bf5b1274d8860bc949a50e7c99b4
kenneth <kjgoins.jg@gmail.com>
2024-01-18 11:25:24 -0800

    starting styles

--- models/seed.js
+++ models/seed.js
@@ -2,18 +2,21 @@ const teamSeeds = [
     {
       name: 'Team A',
       mascott: 'Lion',
+      photo: "https://3.bp.blogspot.com/-AI2fhTCbWQ4/T26wCp9yk3I/AAAAAAAACTI/6O88_7A0txo/s1600/golden-retriever-picture.jpg",
       city: 'City1',
       state: 'CA'
     },
     {
       name: 'Team B',
       mascott: 'Eagle',
+      photo: "https://www.mystart.com/blog/wp-content/uploads/shutterstock_262571516-e1520615420194.jpg",
       city: 'City2',
       state: 'NY'
     },
     {
       name: 'Team C',
       mascott: 'Bear',
+      photo: "https://www.mybestfrienddogcare.co.uk/wp-content/uploads/2019/12/My-Best-Friend-64-scaled.jpg",
       city: 'City3',
       state: 'TX'
     },
